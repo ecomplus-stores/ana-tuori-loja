7dd6efa19644731eea5eec8e771d07aa853ad9f3
Matheus Reis <35343551+matheusgnreis@users.noreply.github.com>
2021-02-06 18:34:17 -0300

    chore: update layout

--- template/js/custom-js/pages.js
+++ template/js/custom-js/pages.js
@@ -1,1 +1,10 @@
 // Add your custom JavaScript for storefront pages here.
+const toggleClass = (el, className) => el.classList.toggle(className)
+const buttonSearch = document.getElementById('mobile-search-btn')
+buttonSearch.addEventListener('click', function (e) {
+  if (window.innerWidth > 767) {
+    toggleClass(document.querySelector('.header .d-none.d-lg-block.col-12'), 'hide')
+  } else {
+    toggleClass(document.querySelector('.header .d-lg-block.col-12'), 'hide')
+  }
+})
